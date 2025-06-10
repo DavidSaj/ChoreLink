0c383df49010a31350bacc62750340b9c7375044
DavidSaj <139820288+DavidSaj@users.noreply.github.com>
2025-06-10 23:22:25 +0200

    reverting some changes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,5 @@
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import React from 'react';
-import { View } from "react-native";
 import CalendarPage from '../../screens/CalendarPage';
 import MainPage from '../../screens/MainPage';
 
@@ -8,15 +7,13 @@ const Tab = createMaterialTopTabNavigator();
 
 export default function App() {
   return (
-    <View style={{ flex: 1 }}>
-      <Tab.Navigator
-        screenOptions={{ swipeEnabled: true }}
-        tabBar={() => null} // Hides the top tab bar
-      >
-        <Tab.Screen name="Home" component={MainPage} />
-        <Tab.Screen name="Calendar" component={CalendarPage} />
-        {/* Add more pages here */}
-      </Tab.Navigator>
-    </View>
+    <Tab.Navigator
+      screenOptions={{ swipeEnabled: true }}
+      tabBar={() => null} // Hides the top tab bar
+    >
+      <Tab.Screen name="Home" component={MainPage} />
+      <Tab.Screen name="Calendar" component={CalendarPage} />
+      {/* Add more pages here */}
+    </Tab.Navigator>
   );
 }
